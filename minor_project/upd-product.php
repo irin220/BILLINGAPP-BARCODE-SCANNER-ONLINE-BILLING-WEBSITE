5985632dd762af0f3d53c7500eae4b2f005c2ea5
Irin Khan <154420448+irin220@users.noreply.github.com>
2024-06-03 21:44:54 +0530

    Files

---
+++ minor_project/upd-product.php
@@ -0,0 +1,15 @@
+<!-- upd-product.php -->
+
+<?php
+include("inc/db.php");
+if (isset($_POST["save"])) {
+    $id=$_POST['id'];
+    $pn = $_POST['pname'];
+    $pr = $_POST['pprice'];
+    $pb = $_POST['pbar'];
+    $ins = "UPDATE products SET pname='$pn',pprice='$pr',barcode='$pb' WHERE pid='$id'";
+    if ($con->query($ins)) {
+        header("location:list-product.php");
+    }
+}
+?>
